b8dcf43e4eb4410601905ac11f1e95986a80b1ce
Szymon Kozlowski <szymonex@vp.pl>
2018-07-26 12:00:58 +0200

    Navigation

---
+++ Assets/DragonBoar/Scripts/Unit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //w tej bibliotece mamy nawigacje
+
+public class Unit : MonoBehaviour {
+
+    const string ANIMATOR_SPEED = "Speed",
+        ANIMATOR_ALIVE = "Alive",
+        ANIMATOR_ATTACK = "Attack";
+
+    public Transform target;
+
+    NavMeshAgent nav;
+    Animator animator;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target)
+        {
+            nav.SetDestination(target.position);
+        }
+        Animate();
+
+	}
+
+    protected virtual void Animate()
+    {
+        var speedVector = nav.velocity;
+        speedVector.y = 0; //po to zeby wyzerowac y ktory czesto jest dosc przypadkowy i moze namieszac w animacji
+
+        float speed = speedVector.magnitude;
+
+        animator.SetFloat(ANIMATOR_SPEED, speed);
+    }
+
+    
+}
